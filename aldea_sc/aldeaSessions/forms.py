9302e431a545f156a93556b854a661c796de6bbf
guillen <oguillen@turpialdev.com>
2016-08-12 02:15:45 -0400

    commit 2

---
+++ aldea_sc/aldeaSessions/forms.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from django import forms
+from django.contrib.auth.models import User
+from django.utils.http import urlsafe_base64_encode
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.sites.shortcuts import get_current_site
+from django.contrib.auth import get_user_model
+from django.utils.encoding import force_bytes
+from django.template import Context
+from django.template.loader import get_template
+from django.core.mail import EmailMessage
+from random import choice
+from string import ascii_lowercase, digits
+
+
+def generate_random_username(length=16, chars=ascii_lowercase + digits, split=4, delimiter='-'):
+
+    username = ''.join([choice(chars) for i in xrange(length)])
+
+    if split:
+        username = delimiter.join(
+            [username[start:start+split] for start in range(0, len(username), split)])
+
+    try:
+        User.objects.get(username=username)
+        return generate_random_username(
+            length=length, chars=chars, split=split, delimiter=delimiter)
+    except User.DoesNotExist:
+        return username
+
+
+class PasswordResetForm(forms.Form):
+    email = forms.EmailField(label=_("Email"), max_length=254)
+
+    def save(self, domain_override=None,
+             subject_template_name='registration/password_reset_subject.txt',
+             email_template_name='registration/password_reset_email.html',
+             use_https=False, token_generator=default_token_generator,
+             from_email=None, request=None, html_email_template_name=None):
+        """
+        Generates a one-use only link for resetting password and sends to the
+        user.
+        """
+        email = self.cleaned_data["email"]
+        active_users = User.objects.filter(
+            email=email, is_active=True)
+        for user in active_users:
+            # Make sure that no email is sent to a user that actually has
+            # a password marked as unusable
+            if not user.has_usable_password():
+                continue
+            if not domain_override:
+                current_site = get_current_site(request)
+                site_name = current_site.name
+                domain = current_site.domain
+            else:
+                site_name = domain = domain_override
+            c = {
+                'email': user.email,
+                'domain': domain,
+                'site_name': site_name,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'user': user,
+                'token': token_generator.make_token(user),
+                'protocol': 'https' if use_https else 'http',
+            }
+            subject = 'Reestablece tu contraseña'
+            # Email subject *must not* contain newlines
+            message = get_template(
+                'registrations/html_password_reset_email.html').render(Context(c))
+            msg = EmailMessage(
+                subject, message, to=[user.email], from_email=from_email)
+            msg.content_subtype = 'html'
+            msg.send()
+
+
+class UserCreateForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = ("first_name", "last_name", "email")
+
+        error_messages = {
+            'first_name': {
+                'required': "Este campo es requerido"
+            },
+            'last_name': {
+                'required': "Este campo es requerido"
+            }
+        }
+
+        widgets = {
+            'email': forms.TextInput(attrs={'required': 'true'}),
+            'first_name': forms.TextInput(attrs={'required': 'true'}),
+            'last_name': forms.TextInput(attrs={'required': 'true'})
+        }
+
+        labels = {
+            'email': 'Correo',
+            'first_name': 'Nombre',
+            'last_name': 'Apellido',
+        }
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).count() != 0:
+            raise forms.ValidationError(u'Este correo ya existe.')
+        return email
+
+    def save(self, commit=True):
+        user = super(UserCreateForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        user.username = generate_random_username()
+        user.is_active = 0
+        password = User.objects.make_random_password()
+        user.set_password(password)
+        user.save()
+        return user
+
+
+class LoginForm(forms.Form):
+
+    username = forms.CharField(
+        max_length=60, required=True,
+        label='', widget=forms.TextInput(attrs={'placeholder': 'Email'}))
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={'placeholder': 'Clave'}), required=True, label='')
---
+++ aldea_sc/aldeaSessions/views.py
@@ -0,0 +1,165 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import hashlib
+import datetime
+import random
+from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response, render, get_object_or_404
+from django.template import RequestContext
+from django.contrib.auth import *
+from django.core.urlresolvers import reverse_lazy
+from django.core.mail import EmailMessage
+from django.utils import timezone
+from django.contrib.auth.models import User, Group
+from django.views import generic
+from aldeaSessions.forms import *
+from django.template import Context
+from django.template.loader import get_template
+from django.contrib.auth.tokens import default_token_generator
+from django.views.decorators.csrf import csrf_protect
+from django.template.response import TemplateResponse
+from django.views.defaults import page_not_found
+
+
+def user_registration(request):
+    if request.user.is_authenticated():
+        # We obtain the user group by the user logged.
+        # Sellers will created by agents
+        # Agents will created by admins
+        if request.method == 'POST':
+            form = UserCreateForm(request.POST)
+
+            if form.is_valid():
+                my_user = form.save()
+                username = my_user.username
+                email = form.cleaned_data['email']
+                salt = hashlib.sha1(str(random.random())).hexdigest()[:5]
+                activation_key = hashlib.sha1(salt + email).hexdigest()
+                key_expires = datetime.datetime.today() +\
+                    datetime.timedelta(2)
+                user = User.objects.get(username=username)
+                new_profile = UserProfile(user=user,
+                                          activation_key=activation_key,
+                                          key_expires=key_expires)
+                new_profile.save()
+                group = Group.objects.get(name='profesional')
+                user.groups.add(group)
+
+                return HttpResponseRedirect(
+                    reverse_lazy('login'))
+            else:
+                context = {'form': form}
+                return render_to_response(
+                    'registro_usuario.html', context,
+                    context_instance=RequestContext(request))
+        else:
+            form = UserCreateForm()
+            context = {'form': form}
+            return render_to_response(
+                'registro_usuario.html', context,
+                context_instance=RequestContext(request))
+    else:
+        return HttpResponseRedirect(
+            reverse_lazy('login'))
+
+
+def authenticate_user(username=None, password=None):
+        """ Authenticate a user based on email address as the user name. """
+        try:
+            user = User.objects.get(email=username)
+            if user is not None:
+                return user
+        except User.DoesNotExist:
+            try:
+                user = User.objects.get(username=username)
+                if user is not None:
+                    return user
+            except User.DoesNotExist:
+                return None
+
+
+def login_request(request):
+    if request.user.is_authenticated():
+        return HttpResponseRedirect(
+            reverse_lazy('vehiculo'))
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user_auth = authenticate_user(username, password)
+            if user_auth is not None:
+                user = authenticate(username=user_auth.username,
+                                    password=password)
+                if user:
+                    login(request, user)
+                    return HttpResponseRedirect(
+                        reverse_lazy('vehiculo'))
+                else:
+                    form.add_error(
+                        None, "Tu correo o contraseña no son correctos")
+            else:
+                form.add_error(
+                    None, "Tu correo o contraseña no son correctos")
+    else:
+        form = LoginForm()
+    context = {'form': form, 'host': request.get_host()}
+    return render_to_response('login.html', context,
+                              context_instance=RequestContext(request))
+
+
+def generate_key(request, pk):
+
+    if request.user.is_authenticated():
+        HttpResponseRedirect(reverse_lazy('login'))
+
+    user = User.objects.get(pk=pk)
+    UserProfile.objects.filter(user=user).delete()
+    salt = hashlib.sha1(str(random.random())).hexdigest()[:5]
+    activation_key = hashlib.sha1(salt + user.email).hexdigest()
+    key_expires = datetime.datetime.today() + datetime.timedelta(2)
+    new_profile = UserProfile(user=user, activation_key=activation_key,
+                              key_expires=key_expires)
+    new_profile.save()
+    return render_to_response('registro_usuario.html')
+
+
+@csrf_protect
+def password_reset(request, is_admin_site=False,
+                   template_name='registration/password_reset_form.html',
+                   email_template_name='registration/password_reset_email.html',
+                   subject_template_name='registration/password_reset_subject.txt',
+                   password_reset_form=PasswordResetForm,
+                   token_generator=default_token_generator,
+                   post_reset_redirect=None,
+                   from_email=None,
+                   current_app=None,
+                   extra_context=None,
+                   html_email_template_name=None):
+    post_reset_redirect = reverse_lazy('password_reset_done')
+    if request.method == "POST":
+        form = password_reset_form(request.POST)
+        if form.is_valid():
+            opts = {
+                'use_https': request.is_secure(),
+                'token_generator': token_generator,
+                'from_email': from_email,
+                'email_template_name': email_template_name,
+                'subject_template_name': subject_template_name,
+                'request': request,
+                'html_email_template_name': html_email_template_name,
+            }
+            if is_admin_site:
+                opts = dict(opts, domain_override=request.get_host())
+            form.save(**opts)
+            return HttpResponseRedirect(post_reset_redirect)
+    else:
+        form = password_reset_form()
+    context = {
+        'form': form,
+        'title': _('Password reset'),
+    }
+    if extra_context is not None:
+        context.update(extra_context)
+    return TemplateResponse(request, template_name, context,
+                            current_app=current_app)
