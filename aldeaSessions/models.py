911528ea0a95d95443309bfd3460be5727ffed69
Emanuel Sánchez <emanuelsab@gmail.com>
2018-04-12 23:17:14 -0400

    resolved issue with datetime

--- aldeaSessions/models.py
+++ aldeaSessions/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
-import datetime
+from datetime import datetime
 
 
 class UserProfile(models.Model):
 	user = models.OneToOneField(User)
 	activation_key = models.CharField(max_length=40, blank=True)
-	key_expires = models.DateTimeField(default=datetime.date.today())
+	key_expires = models.DateTimeField(default=datetime.now)
 
 	def __str__(self):
 		return self.user.username
