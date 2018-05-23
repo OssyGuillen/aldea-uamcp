e2a0fe8ddacec01c3e081a24a26bda57f9cad660
Christhian Guevara <christhian@thesocialus.com>
2018-05-23 00:43:27 -0400

    Cambios varios

--- aldea/urls.py
+++ aldea/urls.py
@@ -14,26 +14,25 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.views.generic import TemplateView
-
+from django.views.generic import TemplateView, ListView
+from blog.models import Noticia
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.conf import settings
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='blog/home.html'), name='home'),
-    url(r'^aldea/$', TemplateView.as_view(template_name='blog/base_aldea.html'), name='aldea'),
-    url(r'^contacto/$', TemplateView.as_view(template_name='blog/contact.html'), name='contacto'),
-    url(r'^nosotros/$', TemplateView.as_view(template_name='blog/doctors.html'), name='nosotros'),
-    url(r'^noticias/$', TemplateView.as_view(template_name='blog/blog.html'), name='noticias'),
-    url(r'^preguntas/$', TemplateView.as_view(template_name='blog/preguntas_frecuentes.html'), name='preguntas'),
+    url(r'^$', TemplateView.as_view(template_name='blog/inicio.html'), name='inicio'),
+    url(r'^nosotros/$', TemplateView.as_view(template_name='blog/nosotros.html'), name='nosotros'),
+    url(r'^noticias/$', ListView.as_view(template_name='blog/noticias.html', model=Noticia), name='noticias'),
     url(r'^recursos/alimentacion/$', TemplateView.as_view(template_name='blog/alimentacion.html'), name='alimentacion'),
-    url(r'^recursos/alimentacion/gluten/$', TemplateView.as_view(template_name='blog/gluten.html'), name='gluten'),
-    url(r'^recursos/alimentacion/lacteos/$', TemplateView.as_view(template_name='blog/lacteos.html'), name='lacteos'),
     url(r'^recursos/alimentacion/recetas/$', TemplateView.as_view(template_name='blog/recetas.html'), name='recetas'),
+    url(r'^recursos/alimentacion/lacteos/$', TemplateView.as_view(template_name='blog/lacteos.html'), name='lacteos'),
+    url(r'^recursos/alimentacion/gluten/$', TemplateView.as_view(template_name='blog/gluten.html'), name='gluten'),
+    url(r'^recursos/peliculas/$', TemplateView.as_view(template_name='blog/peliculas.html'), name='peliculas'),
     url(r'^recursos/centros/$', TemplateView.as_view(template_name='blog/centros.html'), name='centros'),
     url(r'^recursos/enlaces/$', TemplateView.as_view(template_name='blog/enlaces.html'), name='enlaces'),
-    url(r'^recursos/libro/$', TemplateView.as_view(template_name='blog/libro.html'), name='libro'),
     url(r'^recursos/manual/$', TemplateView.as_view(template_name='blog/manual.html'), name='manual'),
-    url(r'^recursos/peliculas/$', TemplateView.as_view(template_name='blog/peliculas.html'), name='peliculas'),
+    url(r'^recursos/libro/$', TemplateView.as_view(template_name='blog/libro.html'), name='libro'),
+    url(r'^preguntas/$', TemplateView.as_view(template_name='blog/preguntas.html'), name='preguntas'),
+    url(r'^contacto/$', TemplateView.as_view(template_name='blog/contacto.html'), name='contacto'),
+    url(r'^aldea/$', TemplateView.as_view(template_name='blog/aldea.html'), name='aldea'),
     url(r'^admin/', include(admin.site.urls)),
 ] + staticfiles_urlpatterns()
